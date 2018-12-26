8386d0f64b270a99020380bb83ee1602ce7d7bfe
Amar Nath PATHAK <amarnath.pathak@amadeus.com>
2018-12-26 18:19:41 +0530

    Adding files

---
+++ src/main/java/com/amadeus/home/App.java
@@ -0,0 +1,13 @@
+package com.amadeus.home;
+
+/**
+ * Hello Home!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Home!" );
+    }
+}
